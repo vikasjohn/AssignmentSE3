e4e5e2fe7179ac17d17ad9ea8cdbb904fef0b962
vikasjohn <vikas.john@outlook.com>
2017-02-28 23:50:33 +0000

    trying to source the file.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,36 @@
 import argparse
 import urllib.request
 import re 
+import numpy as np
+from textwrap import fill
 
-
-
+''' reading the file  form the src file'''
 filename = "input_assign3.txt"
 print(filename)
 with open(filename) as f:
-    line = f.readline()
+    line = f.readline(25)
     value = line.strip().split()
-    print(line)
-    print(value)
-x = int(line)
-print(x)
+#    print(line)
+#   print(value)
+#x = int(line)
+#print(x)
+a2d = [ list(range(i*10, i*10 + 10)) for i in range(10) ]
+# print(a2d)
+
+L = int(line)
+print(L)
+#grid = [[False] * 10 for i in range(10)]
+#ledGrid = np.array(grid)
+#print(grid)
+#print(ledGrid)
+
+#grid = 0
+#fill = False
+#for i in range(L):
+ #   for j in range(L):
+#        grid [i][j] = fill
+        
+#print(grid)
+    
+Grid_Array=[[0]*L for _ in range(L)]
+#print(Grid_Array)
